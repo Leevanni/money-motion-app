d66e8ca4641b60323fa06fe0d64894dcf7220a36
Leevanni Goncalves <leevannigoncalves@gmail.com>
2026-04-10 17:03:22 -0400

    adding primeng

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,10 +1,11 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Transaction } from './component/transaction/transaction';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,11 +14,12 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    provideAnimations(),
+    provideHttpClient()
   ],
   bootstrap: [App]
 })
